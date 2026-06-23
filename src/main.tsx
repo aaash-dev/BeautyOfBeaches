4d17eb704a791b32a3f0f002e5b1dd5a2c4a3963
aaash <aya.a.afifi@gmail.com>
2026-06-23 15:31:57 +0300

    fixed an error

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 // Root entrypoint for the SPA: mount the React App and import the shared global stylesheet once.  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
+import { createRoot } from "react-dom/client";  
+import App from "./app/App.tsx";
   import "./styles/index.css";
 
   createRoot(document.getElementById("root")!).render(<App />);
